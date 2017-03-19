d0647a293ee5689f1c8b5c6bb085bf439b6e15e5
Chabare <chabare95@gmail.com>
2017-03-19 23:02:28 +0100

    Add DBTable

---
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/DBTable.java
@@ -0,0 +1,64 @@
+package com.github.bachelorpraktikum.dbvisualization.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class DBTable {
+
+    private Tables table;
+    private String select_query_string = "SELECT %s FROM %s;";
+    private PreparedStatement select_query_statement;
+    private ResultSet select_result;
+
+    public DBTable(Connection connection, Tables table) throws SQLException {
+        this.table = table;
+        select_query_string = String
+            .format(select_query_string, getColumnNamesAsString(), getName());
+        createSelectStatement(connection);
+    }
+
+    DBTable() {
+    }
+
+    private void createSelectStatement(Connection connection) throws SQLException {
+        select_query_statement = connection.prepareStatement(select_query_string);
+    }
+
+    /**
+     * Executes a select query over all column names (`getColumnNamesAsString`)
+     *
+     * @return ResultSet for 'SELECT `getColumnNamesAsString` FROM `getName()`'
+     */
+    public ResultSet select() throws SQLException {
+        return select(false);
+    }
+
+    ResultSet select(boolean update) throws SQLException {
+        if (select_result != null && !update) {
+            return select_result;
+        }
+        select_result = select_query_statement.executeQuery();
+        return select_result;
+    }
+
+    List<String> getColumnNames() {
+        return table.getColumnNames();
+    }
+
+    /**
+     * Returns a comma seperated list of column names.
+     * Column names are retrieved by `getColumnNames`.
+     *
+     * @return Column names (comma seperated)
+     */
+    String getColumnNamesAsString() {
+        return getColumnNames().stream().reduce((s, s1) -> s + ", " + s1).get();
+    }
+
+    String getName() {
+        return table.getName();
+    }
+}
