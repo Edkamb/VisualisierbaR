ee498506ec598646cfd39a61d5fe3eb14f525c78
Chabare <chabare95@gmail.com>
2017-03-20 18:43:48 +0100

    Add numbericalValue and retrieving possibility to direction

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Direction.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Direction.java
@@ -1,18 +1,41 @@
 package com.github.bachelorpraktikum.dbvisualization.database;
 
+import sun.plugin.dom.exception.InvalidStateException;
+
 public enum Direction {
-    None("None"),
-    AscendingChainage("Ascending"),
-    DescendingChainage("Descending"),
-    Both("Both");
+    None("None", 0),
+    AscendingChainage("Ascending", 1),
+    DescendingChainage("Descending", 2),
+    Both("Both", 3);
 
     private String name;
+    private int numericalValue;
 
-    Direction(String name) {
+    Direction(String name, int numericalValue) {
         this.name = name;
+        this.numericalValue = numericalValue;
     }
 
     String getName() {
         return name;
+    }
+
+    int numericalValue() {
+        return numericalValue;
+    }
+
+    public static Direction get(int index) {
+        if (index > Direction.values().length) {
+            throw new InvalidStateException(
+                "Index can't be larger than the length of the Direction enum.");
+        }
+
+        for (Direction dir : Direction.values()) {
+            if (dir.numericalValue() == index) {
+                return dir;
+            }
+        }
+
+        return null;
     }
 }
