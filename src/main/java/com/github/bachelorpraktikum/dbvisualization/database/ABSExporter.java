671ebcbc988fb3b044b49e13f98d656e533ea729
Chabare <chabare95@gmail.com>
2017-03-21 15:58:07 +0100

    Add ABSExporter

---
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/ABSExporter.java
@@ -0,0 +1,39 @@
+package com.github.bachelorpraktikum.dbvisualization.database;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class ABSExporter implements ABSExportable {
+
+    private final List<ABSExportable> elements;
+
+    ABSExporter(List<ABSExportable> elements) {
+        this.elements = elements;
+    }
+
+    private String getEnclosingFunctionFormattable() {
+        String lineSep = System.lineSeparator();
+        return String.format("Unit run() {%s%s%s%s}", lineSep, "%s", "%s", lineSep);
+    }
+
+    private String getLineSeperatedElements() {
+        StringJoiner lineSeperatorJoiner = new StringJoiner(System.lineSeparator());
+        elements.forEach(absExportable -> lineSeperatorJoiner.add(absExportable.export()));
+
+        return lineSeperatorJoiner.toString();
+    }
+
+    private String getExtra() {
+        return "";
+    }
+
+    @Override
+    public String export() {
+        return String
+            .format(getEnclosingFunctionFormattable(), getLineSeperatedElements(), getExtra());
+    }
+
+    public List<ABSExportable> getElements() {
+        return elements;
+    }
+}
