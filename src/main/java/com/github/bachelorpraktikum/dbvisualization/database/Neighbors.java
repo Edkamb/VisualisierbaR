5d7f8bbfadde02bcffa3eb5ef8c60521856ba426
Chabare <chabare95@gmail.com>
2017-03-24 14:29:50 +0100

    Adapt neighbor to new database model

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Neighbors.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Neighbors.java
@@ -3,48 +3,56 @@ package com.github.bachelorpraktikum.dbvisualization.database;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Iterator;
-import java.util.Optional;
 
 public class Neighbors {
 
     private final int id;
-    private DBEdge edge;
-    private final int edgeID;
-    private Betriebsstelle betriebsstelle;
-    private final int betriebsstelleID;
+    private final int vertex1ID;
+    private final int vertex2ID;
+    private Vertex vertex1;
+    private Vertex vertex2;
 
-    public Neighbors(int id, DBEdge edge, Betriebsstelle betriebsstelle) {
+    public Neighbors(int id, Vertex vertex1, Vertex vertex2) {
         this.id = id;
-        this.edge = edge;
-        edgeID = edge.getId();
-        this.betriebsstelle = betriebsstelle;
-        betriebsstelleID = betriebsstelle.getId();
+        this.vertex1 = vertex1;
+        vertex1ID = vertex1.getId();
+        this.vertex2 = vertex2;
+        vertex2ID = vertex2.getId();
     }
 
     Neighbors(ResultSet rs) throws SQLException {
         Iterator<String> columnNames = Tables.NEIGHBORS.getColumnNames().iterator();
         id = rs.getInt(columnNames.next());
-        edgeID = rs.getInt(columnNames.next());
-        betriebsstelleID = rs.getInt(columnNames.next());
+        vertex1ID = rs.getInt(columnNames.next());
+        vertex2ID = rs.getInt(columnNames.next());
     }
 
     public int getId() {
         return id;
     }
 
-    public Optional<DBEdge> getEdge() {
-        return Optional.ofNullable(edge);
+
+    public Vertex getVertex1() {
+        return vertex1;
     }
 
-    public Optional<Betriebsstelle> getBetriebsstelle() {
-        return Optional.ofNullable(betriebsstelle);
+    public void setVertex1(Vertex vertex1) {
+        this.vertex1 = vertex1;
     }
 
-    public int getEdgeID() {
-        return edgeID;
+    public Vertex getVertex2() {
+        return vertex2;
     }
 
-    public int getBetriebsstelleID() {
-        return betriebsstelleID;
+    public void setVertex2(Vertex vertex2) {
+        this.vertex2 = vertex2;
+    }
+
+    public int getVertex1ID() {
+        return vertex1ID;
+    }
+
+    public int getVertex2ID() {
+        return vertex2ID;
     }
 }
