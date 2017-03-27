f0ea59d3969b65960b482b0692b40c0af0745ef9
Chabare <chabare95@gmail.com>
2017-03-27 19:22:58 +0200

    WIP: Add export

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Attribute.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/Attribute.java
@@ -2,8 +2,11 @@ package com.github.bachelorpraktikum.dbvisualization.database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.StringJoiner;
 
 public class Attribute extends DBTable implements ABSExportable, Element {
 
@@ -11,6 +14,8 @@ public class Attribute extends DBTable implements ABSExportable, Element {
     private final String title;
     private final String description;
     private final String acronym;
+    private Vertex vertex;
+    private DBEdge edge;
 
     public Attribute(int id, String title, String description, String acronym) {
         this.id = id;
@@ -45,7 +50,42 @@ public class Attribute extends DBTable implements ABSExportable, Element {
 
     @Override
     public String export() {
-        return null;
+        if (vertex == null) {
+            throw new IllegalArgumentException(
+                "Attribute needs to be tied to a vertex to be exported.");
+        }
+
+        String name = getAbsName();
+        String exportString = "";
+
+        if (getId() == FixAttributeValues.HAUPT_UND_VORSIGNAL.getId()
+            || getId() == FixAttributeValues.HAUPT_UND_VORSIGNAL_MIT_SPERRSIGNAL.getId()) {
+            String hauptsignalFormatString = "HauptSignal %s = new local HauptSignalImpl(%s, %s);";
+            String vorsignalFormatString = "VorSignal %s = new local VorSignalImpl(%s);";
+
+            exportString = String.format(hauptsignalFormatString, name, getVertex().getAbsName(),
+                getEdge().getAbsName());
+            exportString = new StringJoiner(System.lineSeparator()).add(exportString)
+                .add(vorsignalFormatString).toString();
+        } else if () {
+
+        } else {
+            Optional<FixAttributeValues> fixAttributeOpt = FixAttributeValues.get(getId());
+            if (fixAttributeOpt.isPresent()) {
+                FixAttributeValues fixAttribute = fixAttributeOpt.get();
+                String formattableString = "%s %s = new local %s(%s);";
+                exportString = String
+                    .format(formattableString, fixAttribute.classNameLhs(), name,
+                        fixAttribute.classNameRhs(), getVertex().getAbsName());
+                String comment = "// Experimental. This was constructed by using default values. See Attribute.java and FixAttributeValues.java (VisualisierbaR project) for more information.";
+                exportString = new StringJoiner(System.lineSeparator()).add(comment)
+                    .add(exportString).toString();
+            } else {
+                // TODO
+            }
+        }
+
+        return exportString;
     }
 
     @Override
