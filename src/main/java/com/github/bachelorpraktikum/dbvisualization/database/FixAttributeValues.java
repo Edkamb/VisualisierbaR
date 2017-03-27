941cee3dd0183575d95615fd6fdf61f58e7756d4
Chabare <chabare95@gmail.com>
2017-03-27 19:22:45 +0200

    Add lhs/rhs for classNames to FixAttributeValues

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/database/FixAttributeValues.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/FixAttributeValues.java
@@ -1,16 +1,47 @@
 package com.github.bachelorpraktikum.dbvisualization.database;
 
+import java.util.Optional;
+
 enum FixAttributeValues {
-    ZUGFOLGE(85);
+    ZUGFOLGE(85, "ActiveZugFolge", "ZugFolgeImpl"),
+    HAUPTSIGNAL(1, "HauptSignal", null),
+    HAUPTSIGNAL_MIT_SPERRSIGNAL(2, null, null),
+    VORSIGNAL(3, "VorSignal", null),
+    SPERRSIGNAL(4, "SperrSignal", null),
+    HAUPT_UND_VORSIGNAL(5, null, null),
+    HAUPT_UND_VORSIGNAL_MIT_SPERRSIGNAL(6, null, null);
 
     private final int id;
+    private final String lhs;
+    private final String rhs;
 
-    FixAttributeValues(int id) {
+    FixAttributeValues(int id, String lhs, String rhs) {
         this.id = id;
+        this.lhs = lhs;
+        this.rhs = rhs;
     }
 
     int getId() {
         return id;
+    }
+
+    /**
+     * Returns the left hand side class name for the attribute. If this is null, the attribute can't
+     * be instantiated as one abs element only, it's a composition of different elements. This has
+     * to be handled elsewhere.
+     *
+     * @return Lefthand side class name if is single abs element, null otherwise
+     */
+    String classNameLhs() {
+        return lhs;
+    }
+
+    String classNameRhs() {
+        if (rhs == null) {
+            return String.format("%sImpl", lhs);
+        }
+
+        return rhs;
     }
 
     static Optional<FixAttributeValues> get(int id) {
