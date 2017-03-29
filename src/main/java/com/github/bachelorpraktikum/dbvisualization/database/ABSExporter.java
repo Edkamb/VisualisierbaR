550713afd68445195bc77c921f65b4d6f131ecdf
Chabare <chabare95@gmail.com>
2017-03-29 15:15:33 +0200

    Add export method

--- src/main/java/com/github/bachelorpraktikum/dbvisualization/database/ABSExporter.java
+++ src/main/java/com/github/bachelorpraktikum/dbvisualization/database/ABSExporter.java
@@ -1,11 +1,19 @@
 package com.github.bachelorpraktikum.dbvisualization.database;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.StringJoiner;
 
 public class ABSExporter {
 
     private final List<ABSExportable> elements;
+    private boolean exportString;
+
+    ABSExporter() {
+        this(new LinkedList<>());
+    }
 
     ABSExporter(List<ABSExportable> elements) {
         this.elements = elements;
@@ -27,9 +35,22 @@ public class ABSExporter {
         return "";
     }
 
+    /**
+     * Writes the abs content (enclosed in a `run` function) to the given stream.
+     *
+     * @param outputStream Stream to write abs export to
+     * @throws IOException Thrown if writting to the <code>outputStream</code> was unsuccessful
+     */
+    public void export(OutputStream outputStream) throws IOException {
+        outputStream.write(getExportString().getBytes());
     }
 
     public List<ABSExportable> getElements() {
         return elements;
+    }
+
+    private String getExportString() {
+        return String
+            .format(getEnclosingFunctionFormattable(), getLineSeperatedElements(), getExtra());
     }
 }
